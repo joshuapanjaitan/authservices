95b05962358aef945419d2d34344d1f3ec061065
Joshua <joshuapanjaitan01@gmail.com>
2021-09-07 14:21:10 +0700

    first commit

---
+++ api/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.conf.urls.static import static
+from django.conf import settings
+from . import views
+
+urlpatterns = [
+    path('', views.api_overview, name='services'),
+    path('ulist/', views.userList, name='ulist'),
+    path('auth-list/', views.authentication, name='auth-list'),
+
+]
+
---
+++ api/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.http import JsonResponse, response
+from django.db.models import Q
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from django.contrib.auth.models import Group,User
+from django.contrib.auth import authenticate,login,logout
+
+from api import serializers
+
+# Create your views here.
+# Create your views here.
+@api_view(['GET'])
+def api_overview(request):
+    api_url = {
+        'Auth Login':'/auth-list/',
+        'Auth Register':'/auth-register/',
+
+    }
+    return Response(api_url)
+
+@api_view(['GET'])
+def userList(request):
+    user = User.objects.all()
+    serializer = UserSerializer(user, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def authentication(request):
+    fetch  = request.data
+    try:
+        uname = fetch["username"]
+        pwd = fetch["password"]
+        user = authenticate(request,username=uname, password = pwd)
+        group = user.groups.all()[0].name
+        if user is not None and group == 'management':
+            stat =  User.objects.filter(username = user).values()[0]['is_active'] #get Status activation
+            
+            response = JsonResponse(data= {
+                'message':"Access Successfuly",
+                'status_code':200,
+                'auth_value':stat,
+            })
+            response.status_code = 200
+        elif group != 'management':
+            response = JsonResponse(data= {
+                'message':"Not Authorized",
+                'status_code':501,
+                'auth_value':False,
+            })
+            response.status_code = 501
+        
+        else:
+            response = JsonResponse(data= {
+                'message':"Username or password Incorect",
+                'status_code':501,
+                'auth_value':False,
+            })
+            response.status_code = 501
+
+
+    except:
+        response = JsonResponse(data= {
+            'message':"Something went Wrong, Check your Username or Password",
+            'status_code':501,
+            'data':False,
+        })
+        response.status_code = 501
+
+    return response
